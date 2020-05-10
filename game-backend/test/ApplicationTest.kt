1ef5963bb06b5e55df90b674f02751ec3ae35978
jBosak <jbosak98@gmail.com>
2020-05-10 10:51:22 +0200

    simple test

--- game-backend/test/ApplicationTest.kt
+++ game-backend/test/ApplicationTest.kt
@@ -1,32 +1,9 @@
 package com.wust.game.playing.algorithm.jbosak
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import java.io.*
-import java.util.*
-import io.ktor.network.selector.*
-import io.ktor.network.sockets.*
-import io.ktor.network.util.*
-import kotlin.coroutines.*
-import kotlinx.coroutines.*
-import io.ktor.utils.io.*
-import io.ktor.network.tls.*
-import io.ktor.utils.io.core.*
-import kotlinx.coroutines.*
-import io.ktor.client.*
-import io.ktor.client.engine.cio.*
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.websocket.*
-import io.ktor.http.cio.websocket.*
-import java.time.*
-import io.ktor.client.features.websocket.*
-import io.ktor.client.features.websocket.WebSockets
-import io.ktor.http.cio.websocket.Frame
-import kotlinx.coroutines.channels.*
 import kotlin.test.*
 import io.ktor.server.testing.*
+
 
 class ApplicationTest {
     @Test
--- game-backend/test/algorithm/GetPossibleMovesKtTest.kt
+++ game-backend/test/algorithm/GetPossibleMovesKtTest.kt
@@ -1,14 +1,37 @@
 package algorithm
 
-import createGame
+
+import junit.framework.Assert.assertFalse
 import lib.game.Game
-import model.Team
+import lib.Team.Team
+import lib.pawns.Pawn
+import model.Position
 import org.junit.Before
 import org.junit.Test
 
 internal class GetPossibleMovesKtTest {
 
     lateinit var game: Game
+
+    private fun createGame(): Game {
+        val pawns:Map<Position, Pawn> = mapOf(
+            Position(0,0) to Pawn(0, 0, Team.BLACK),
+            Position(0,4) to Pawn(0, 4, Team.BLACK),
+            Position(0,6) to Pawn(0, 6, Team.BLACK),
+            Position(1,1) to Pawn(1, 1, Team.BLACK),
+            Position(1,3) to Pawn(1, 3, Team.BLACK),
+            Position(2,4) to Pawn(2, 4, Team.WHITE),
+            Position(4,4) to Pawn(4, 4, Team.WHITE),
+            Position(4,2) to Pawn(4, 2, Team.WHITE),
+            Position(2,2) to Pawn(2, 2, Team.WHITE)
+        )
+        return Game(
+            pawns = pawns,
+            nextMove = Team.WHITE,
+            isFinished = false,
+            possibleMoves = getPossibleMoves(pawns)
+        )
+    }
 
     @Before
     fun setUp() {
@@ -17,7 +40,14 @@ internal class GetPossibleMovesKtTest {
 
     @Test
     fun getPossibleMoves() {
-        val x = getPossibleMoves(game.pawns, Team.BLACK)
-        println(game)
+//        val x = getPossibleMoves(game.pawns, Team.BLACK)
+//        println(game)
+    }
+
+    @Test
+    fun filterCapturedPawnTest(){
+        val pawns = filterCapturedPawn(Position(1,3), Position(3,5), game.pawns)
+        assertFalse(pawns.containsKey(Position(2,4)))
+
     }
 }
