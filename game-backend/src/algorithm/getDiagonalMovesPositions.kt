480c2f2bcd4f28f7609bf2746ad58917f7db9593
jBosak <jbosak98@gmail.com>
2020-05-10 12:17:01 +0200

    getCaptureMoves refactor

---
+++ game-backend/src/algorithm/getDiagonalMovesPositions.kt
@@ -0,0 +1,24 @@
+package algorithm
+
+import lib.Team.Team
+import lib.pawns.Pawn
+import model.Position
+
+fun getDiagonalMovesPositions(pawn: Pawn): List<Position> =
+    getDiagonalMovesPositions(Position(pawn.row, pawn.column), pawn.team, pawn.isKing)
+
+fun getDiagonalMovesPositions(position: Position, team: Team, isKing: Boolean = false): List<Position> {
+    val row = position.row + team.teamDirection
+    return when (isKing) {
+        true -> listOf(
+            Position(position.row - 1, position.column - 1),
+            Position(position.row - 1, position.column + 1),
+            Position(position.row + 1, position.column - 1),
+            Position(position.row + 1, position.column + 1)
+        )
+        false -> listOf(
+            Position(row, position.column - 1),
+            Position(row, position.column + 1)
+        )
+    }.filter { isPositionCorrect(it) }
+}
--- game-backend/src/algorithm/getPossibleMoves.kt
+++ game-backend/src/algorithm/getPossibleMoves.kt
@@ -27,19 +27,20 @@ fun getPossibleMovesOfTeam(pawns: Pawns, team: Team): Map<Position, List<Positio
         .getPawns(team)
         .map {
             val moves = getCaptureMoves(it.value, pawns)
-            it.key to moves.filter { (position,_) ->
+            it.key to moves.filter { (position, _) ->
                 !pawns.isFieldOccupied(position) || position == it.key
             }
-        }.fold(listOf<Pair<Position, List<Pair<Position, Int>>>>()){ acc, item:Pair<Position,List<Pair<Position, Int>>> ->
+        }
+        .fold(listOf<Pair<Position, List<Pair<Position, Int>>>>()) { acc, item: Pair<Position, List<Pair<Position, Int>>> ->
             val accKills = acc.maybeFirst()?.second?.maybeFirst()?.second ?: -1
-            val itemKills = item.second.maybeFirst()?.second?: -1
+            val itemKills = item.second.maybeFirst()?.second ?: -1
             when {
                 accKills < itemKills -> listOf(item)
                 accKills == itemKills -> acc + item
                 else -> acc
             }
 
-        }.map {(initialPosition, movesWithKills) ->
+        }.map { (initialPosition, movesWithKills) ->
             initialPosition to movesWithKills.map { (destination, _) -> destination }
         }
         .toMap()
@@ -48,23 +49,37 @@ fun getPossibleMovesOfTeam(pawns: Pawns, team: Team): Map<Position, List<Positio
 }
 
 fun getCaptureMoves(pawn: Pawn, pawns: Pawns): List<Pair<Position, Int>> {
-    return getDiagonalMovesPositions(pawn)
-        .filter { pawns.fieldOccupiedByTeam(it) === pawn.team.opposite() }
-        .map { Position(2 * it.row - pawn.row, 2 * it.column - pawn.column) }
-        .filter { position -> !pawns.isFieldOccupied(position) }
-        .flatMap { movePosition ->
-            val pawnsWithoutCaptured =
-                filterCapturedPawn(pawn.getPosition(), movePosition, pawns)
-            getCaptureMoves(movePosition, pawn.team, pawnsWithoutCaptured, 1)
-        }.fold(emptyList()) { acc: List<Pair<Position, Int>>, item: Pair<Position, Int> ->
-            val itemKills = item.second
-            val accKills = if (acc.isEmpty()) -1 else acc.first().second
-            when {
-                accKills < itemKills -> listOf(item)
-                accKills == itemKills -> acc + item
-                else -> acc
-            }
+    val filterFieldsOccupiedByTheSameTeam = fun(movePosition: Position): Boolean {
+        return pawns.fieldOccupiedByTeam(movePosition) === pawn.team.opposite()
+    }
+
+    val getDestinationPosition = fun(capturedPawnPosition: Position): Position {
+        return Position(2 * capturedPawnPosition.row - pawn.row, 2 * capturedPawnPosition.column - pawn.column)
+    }
+
+    val filterOccupiedFields = fun(position: Position): Boolean {
+        return !pawns.isFieldOccupied(position) && isPositionCorrect(position)
+    }
+
+    val getMultiCaptureMoves = fun(singleCapture: Position): List<Pair<Position, Int>> {
+        val filteredPawns = filterCapturedPawn(pawn.getPosition(), singleCapture, pawns)
+        return getCaptureMoves(singleCapture, pawn.team, filteredPawns, 1)
+    }
+    val getTheLongestCaptures = fun(acc: List<Pair<Position, Int>>, item: Pair<Position, Int>): List<Pair<Position, Int>> {
+        val itemKills = item.second
+        val accKills = if (acc.isEmpty()) -1 else acc.first().second
+        return when {
+            accKills < itemKills -> listOf(item)
+            accKills == itemKills -> acc + item
+            else -> acc
         }
+    }
+    return getDiagonalMovesPositions(pawn)
+        .filter(filterFieldsOccupiedByTheSameTeam)
+        .map (getDestinationPosition)
+        .filter (filterOccupiedFields)
+        .flatMap(getMultiCaptureMoves)
+        .fold(listOf(), getTheLongestCaptures)
 }
 
 fun getCaptureMoves(
@@ -73,18 +88,38 @@ fun getCaptureMoves(
     pawns: Map<Position, Pawn>,
     capturedPawns: Int
 ): List<Pair<Position, Int>> {
-    val possibleMoves = getDiagonalMovesPositions(position, team, true)
-        .filter { pawns.fieldOccupiedByTeam(it) === team.opposite() }
-        .map { Position(2 * it.row - position.row, 2 * it.column - position.column) }
-        .filter { !pawns.isFieldOccupied(it) }
-        .map { Pair(it, capturedPawns + 1) }
-    if (possibleMoves.isEmpty()) return possibleMoves
-    val morePossibleMoves = possibleMoves.flatMap { (move: Position, capturedPawns: Int) ->
+    val filterFieldsOccupiedByTheSameTeam = fun(movePosition: Position): Boolean {
+        return pawns.fieldOccupiedByTeam(movePosition) === team.opposite()
+    }
+    val getDestinationPosition = fun(capturedPawnPosition: Position): Position {
+        return Position(2 * capturedPawnPosition.row - position.row, 2 * capturedPawnPosition.column - position.column)
+    }
+    val filterOccupiedFields = fun(position: Position): Boolean {
+        return !pawns.isFieldOccupied(position) && isPositionCorrect(position)
+    }
+    val getPairOfMoveWithKills = fun(destination: Position): Pair<Position, Int> {
+        return Pair(destination, capturedPawns + 1)
+    }
+
+    val getMultiCaptureMoves = fun(singleCapture: Pair<Position, Int>): List<Pair<Position, Int>> {
+        val (move, kills) = singleCapture
         val filteredPawns = filterCapturedPawn(position, move, pawns)
-        getCaptureMoves(move, team, filteredPawns, capturedPawns)
-    }.toList()
-    return possibleMoves + morePossibleMoves
+        return getCaptureMoves(move, team, filteredPawns, kills)
+    }
+
+    val possibleMoves =
+        getDiagonalMovesPositions(position, team, true)
+            .filter(filterFieldsOccupiedByTheSameTeam)
+            .map(getDestinationPosition)
+            .filter(filterOccupiedFields)
+            .map(getPairOfMoveWithKills)
+
+    val multiCaptureMoves = possibleMoves
+        .flatMap(getMultiCaptureMoves)
+
+    return possibleMoves + multiCaptureMoves
 }
+
 
 fun filterCapturedPawn(position: Position, destination: Position, pawns: Pawns) =
     pawns.filterKeys {
@@ -95,24 +130,6 @@ fun filterCapturedPawn(position: Position, destination: Position, pawns: Pawns) 
         it != capturedPawnPosition && it != position
     }
 
-fun getDiagonalMovesPositions(pawn: Pawn): List<Position> =
-    getDiagonalMovesPositions(Position(pawn.row, pawn.column), pawn.team, pawn.isKing)
-
-fun getDiagonalMovesPositions(position: Position, team: Team, isKing: Boolean = false): List<Position> {
-    val row = position.row + team.teamDirection
-    return when (isKing) {
-        true -> listOf(
-            Position(position.row - 1, position.column - 1),
-            Position(position.row - 1, position.column + 1),
-            Position(position.row + 1, position.column - 1),
-            Position(position.row + 1, position.column + 1)
-        )
-        false -> listOf(
-            Position(row, position.column - 1),
-            Position(row, position.column + 1)
-        )
-    }
-}
 
 fun isPositionCorrect(position: Position): Boolean =
     position.column in 0..7 && position.row in 0..7
