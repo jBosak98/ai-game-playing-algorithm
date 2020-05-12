2edf43d5bcf4d010dd2c9ddcd373b9754a911fa9
jBosak <jbosak98@gmail.com>
2020-05-12 22:26:24 +0200

    Board - format

--- game-frontend/src/components/Board.js
+++ game-frontend/src/components/Board.js
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
-import useWebSocket from 'react-use-websocket';
+import useWebSocket from "react-use-websocket";
 import Pawn from "./Pawn";
 import BoardBackground from "./BoardBackground";
 import "./Board.scss";
-import Loader from './Loader';
-import printConnectionStatus from '../lib/printConnectionStatus';
-import { socketUrl } from '../lib/constants';
-import TurnInfo from './TurnInfo';
+import Loader from "./Loader";
+import printConnectionStatus from "../lib/printConnectionStatus";
+import { socketUrl } from "../lib/constants";
+import TurnInfo from "./TurnInfo";
 
 const Board = ({ row, column, gameMode }) => {
   const [clickedPawn, onPawnClick] = useState(undefined);
-  const [sendMessage, lastMessage, readyState, getWebSocket] = useWebSocket(socketUrl);
-  
+  const [sendMessage, lastMessage, readyState, getWebSocket] = useWebSocket(
+    socketUrl
+  );
+
   printConnectionStatus(readyState);
   const data = parseData(lastMessage);
-  
-  if(!data) return <Loader/>
-  
 
-  const onBoardFieldClick = getOnBoardFieldClick({data, clickedPawn, sendMessage});
+  if (!data) return <Loader />;
+    
+  const onBoardFieldClick = getOnBoardFieldClick({
+    data,
+    clickedPawn,
+    sendMessage,
+  });
   const pawns = getPawns(data);
-  
-  const filteredOnPawnClick = (pawn) => pawn.team === data.nextMove && onPawnClick(pawn)
-  
+
+  const filteredOnPawnClick = (pawn) =>
+    pawn.team === data.nextMove && onPawnClick(pawn);
+
   return (
     <>
-      <TurnInfo nextMove={data.nextMove} gameMode={gameMode}/>
+      <TurnInfo nextMove={data.nextMove} gameMode={gameMode} />
       <div className="Board">
         <BoardBackground
           row={row}
@@ -41,19 +47,25 @@ const Board = ({ row, column, gameMode }) => {
   );
 };
 
-const getOnBoardFieldClick = ({data, clickedPawn, sendMessage}) => (destination) => {
+const getOnBoardFieldClick = ({ data, clickedPawn, sendMessage }) => (
+  destination
+) => {
   const message = {
-    board:data,
-    move:{
-      pawn:clickedPawn,
-      destination
-    }
-  }
+    board: data,
+    move: {
+      pawn: clickedPawn,
+      destination: {
+        row: destination.rowNumber,
+        column: destination.columnNumber,
+      },
+    },
+  };
   sendMessage(JSON.stringify(message));
-}
+};
 
-const parseData = (lastMessage) => lastMessage && lastMessage.data && JSON.parse(lastMessage.data);
+const parseData = (lastMessage) =>
+  lastMessage && lastMessage.data && JSON.parse(lastMessage.data);
 
-const getPawns = (data) => [ ...Object.entries(data.pawns).map(x =>x[1])]
+const getPawns = (data) => [...Object.entries(data.pawns).map((x) => x[1])];
 
 export default Board;
