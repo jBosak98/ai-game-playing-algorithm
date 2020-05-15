359c19574eee831adc88be185be17ec7cfae87d3
jBosak <jbosak98@gmail.com>
2020-05-15 22:56:56 +0200

    frontend - added crown

--- game-frontend/src/components/Pawn.js
+++ game-frontend/src/components/Pawn.js
@@ -1,23 +1,25 @@
 import React from "react";
 
 import { COLOR_BLACK } from "../lib/constants";
-
+import CrownSVG from "../svg/CrownSVG";
 import "./Pawn.scss";
 
 const Pawn = ({ details, onPawnClick }) => {
   const style = getStyleFromPawnDetails(details);
-
+  const isKing = details.isKing;
   return (
     <div className="Pawn" style={style} onClick={() => onPawnClick(details)}>
       <div
         className="fulfillment"
         style={{ backgroundColor: style.isBlack ? "#459" : "#bfa14d" }}
-      ></div>
+      >
+        {isKing && <CrownSVG scale="0.3" transform="scale(0.5 0.5)" />}
+      </div>
     </div>
   );
 };
 
-const getStyleFromPawnDetails = ({ row, column, color, team }) => {
+const getStyleFromPawnDetails = ({ row, column, team }) => {
   const fieldSizePx = 120;
   const isBlack = team === COLOR_BLACK;
   const top = `${row * fieldSizePx}px`;
---
+++ game-frontend/src/svg/CrownSVG.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CrownSVG = (props) => (
+  <svg version="1.0" width="128" height="128" {...props}>
+    <path
+      d="M64 31.485l28.902 36.128 28.903-36.128-7.226 65.03H13.421l-7.226-65.03 28.903 36.128L64 31.485z"
+      fill="#ffcd00"
+      fill-rule="evenodd"
+      stroke="#000"
+      stroke-width="4.335"
+    />
+  </svg>
+);
+
+export default CrownSVG;
